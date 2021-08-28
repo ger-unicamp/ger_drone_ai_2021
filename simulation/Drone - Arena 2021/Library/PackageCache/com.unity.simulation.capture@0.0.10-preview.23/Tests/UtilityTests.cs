8f0078c224b7a51da386249032e95c9f5acecfe8
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-08-28 18:30:22 -0300

    Projeto da arena criado

---
+++ simulation/Drone - Arena 2021/Library/PackageCache/com.unity.simulation.capture@0.0.10-preview.23/Tests/UtilityTests.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+
+using UnityEngine;
+
+using Unity.Simulation;
+
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class UtilityTests
+{
+    [Test]
+    public void ArrayCast_CastingToOtherArrayTypesDoesNotCopy()
+    {
+        foo();
+    }
+
+    [UnityTest]
+    public IEnumerator UnityVersionParsingTest()
+    {
+        var currentUnityVersion = Application.unityVersion;
+
+        Assert.IsTrue(GeneralUtilities.IsUnityVersionGreaterThanEqualTo(currentUnityVersion));
+        Assert.IsTrue(GeneralUtilities.IsUnityVersionGreaterThanEqualTo("2019.3.2f1"));
+        Assert.IsFalse(GeneralUtilities.IsUnityVersionGreaterThanEqualTo("2021.1.2f1"));
+
+        yield return null;
+    }
+
+    void foo()
+    {
+        const int kElementCount = 20000;
+
+        var byteArray    = new byte[kElementCount];
+        var intArray     = ArrayUtilities.Cast<int>(byteArray);
+        var floatArray   = ArrayUtilities.Cast<float>(byteArray);
+        var colorArray   = ArrayUtilities.Cast<Color>(byteArray);
+        var color32Array = ArrayUtilities.Cast<Color32>(byteArray);
+        var vector2Array = ArrayUtilities.Cast<Vector2>(byteArray);
+        var vector3Array = ArrayUtilities.Cast<Vector3>(byteArray);
+        var vector4Array = ArrayUtilities.Cast<Vector4>(byteArray);
+
+        Debug.Assert(Object.ReferenceEquals(byteArray, intArray));
+        Debug.Assert(Object.ReferenceEquals(intArray, colorArray));
+        Debug.Assert(Object.ReferenceEquals(colorArray, color32Array));
+        Debug.Assert(Object.ReferenceEquals(color32Array, vector2Array));
+        Debug.Assert(Object.ReferenceEquals(vector2Array, vector3Array));
+        Debug.Assert(Object.ReferenceEquals(vector3Array, vector4Array));
+
+        Debug.Assert(ArrayUtilities.Count<float>(floatArray)     == kElementCount / sizeof(float));
+        Debug.Assert(ArrayUtilities.Count<int>(intArray)         == kElementCount / sizeof(int));
+        Debug.Assert(ArrayUtilities.Count<Color>(colorArray)     == kElementCount / Marshal.SizeOf(typeof(Color)));
+        Debug.Assert(ArrayUtilities.Count<Color32>(color32Array) == kElementCount / Marshal.SizeOf(typeof(Color32)));
+        Debug.Assert(ArrayUtilities.Count<Vector2>(vector2Array) == kElementCount / Marshal.SizeOf(typeof(Vector2)));
+        Debug.Assert(ArrayUtilities.Count<Vector3>(vector3Array) == kElementCount / Marshal.SizeOf(typeof(Vector3)));
+        Debug.Assert(ArrayUtilities.Count<Vector4>(vector4Array) == kElementCount / Marshal.SizeOf(typeof(Vector4)));
+    }
+}
---
+++ simulation/Drone - Arena 2021/Library/PackageCache/com.unity.simulation.core@0.0.10-preview.25/Runtime/PerfStatsManager.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Unity.Simulation;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Unity.Simulation
+{
+    public class PerfStatsManager
+{
+    private PerfStatsManager()
+    {
+    }
+
+    private static PerfStatsManager _instance;
+
+    internal List<CollectorBase> collectors;
+    internal List<IGlobalProfilerDataDispatcher> globalDispatchers;
+
+    public double DEFAULT_ERROR_SAMPLE_VALUE = Double.NegativeInfinity;
+    public float realTimeSinceStartUp;
+    
+    
+
+    /// <summary>
+    /// PerfStatsManager singleton instance to that can enable Start/Stop invoking the collectors. 
+    /// </summary>
+    public static PerfStatsManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new PerfStatsManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void SetGlobalDispatchers(List<IGlobalProfilerDataDispatcher> dispatchers)
+    {
+        globalDispatchers = dispatchers;
+    }
+    
+    /// <summary>
+    /// Invoke this method to start profiling by invoking/activating collectors.
+    /// </summary>
+    public void StartProfiling(List<CollectorBase> collectors)
+    {
+        this.collectors = collectors;
+        foreach (var collector in this.collectors)
+        {
+            collector.Initialize();
+            collector.PopulateEvents();
+        }
+
+        Manager.Instance.Tick += (float dt) =>
+        {
+            realTimeSinceStartUp = Time.realtimeSinceStartup;
+            foreach (var c in collectors)
+            {
+                foreach (var e in c.GetEvents())
+                {
+                    e.Update(dt);
+                }
+            }
+            InvokeCollectors(dt);
+        };
+        Manager.Instance.ShutdownNotification += () => DispatchCollectors(true);
+    }
+
+    /// <summary>
+    /// Inovke this method to stop profiling.
+    /// </summary>
+    public void StopProfiling() => Manager.Instance.Tick -= InvokeCollectors;
+
+    private void InvokeCollectors(float dt)
+    {
+        //Update all the count
+        Debug.Assert(collectors!=null, "No collectors are assigned.");
+        UpdateElapsedTimeOnCollectors(dt);
+        DispatchCollectors();
+    }
+
+    private void UpdateElapsedTimeOnCollectors(float dt)
+    {
+        foreach (var collector in collectors)
+        {
+            collector.simulationElapsedTime += dt;
+        }
+    }
+
+    /// <summary>
+    /// Invoke this function to flush all the events on all the collectors. This will invoke dispatcher on all the collectors.
+    /// This call can be slow depending upon the dispatcher implementation.
+    /// </summary>
+    /// <param name="shuttingDown"></param>
+    public void DispatchCollectors(bool shuttingDown = false)
+    {
+        foreach (var collector in collectors)
+        {
+            if ((collector.m_Enabled && collector.simulationElapsedTime >= collector.period) || shuttingDown)
+            {
+                collector.simulationElapsedTime = 0;
+                var events = collector.GetEvents();
+                var report = ReportingUtils.GetProfilerReport(events, collector.name, collector.category);
+                if (globalDispatchers != null && globalDispatchers.Count > 0)
+                {
+                    foreach(var dispatcher in globalDispatchers)
+                        dispatcher.DispatchProfilerReport(report);
+                }
+                else
+                {
+                    collector.DispatchCollector(report);
+                    collector.CollectorConsumer?.Invoke(report);   
+                }
+                foreach (var e in events)
+                    if (e.resetOnEachSampleCollection)
+                        e.Reset();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Track API usage for the provided function.
+    /// </summary>
+    /// <param name="functionName"></param>
+    /// <param name="associatedColletorType"></param>
+    public void TrackAPIUsage(string functionName, Type associatedColletorType)
+    {
+        if (collectors == null)
+            return;
+        var collector = collectors.FirstOrDefault(c => c.GetType() == associatedColletorType);
+        if (collector != null)
+        {
+            var events = collector.GetEvents();
+            var counterEvent = (CounterEvent)events.FirstOrDefault(e => e.GetType() == typeof(CounterEvent));
+            if (counterEvent == null)
+            {
+                Log.E(
+                    "[Simulation Profiler]: Counter Event is not registered with the collector. Please register the event by call AddEvent in your collector.",
+                    true);
+            }
+            else
+            {
+                counterEvent.IncrementCount(functionName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Begin sample for the provided event.
+    /// </summary>
+    /// <param name="functionName"></param>
+    public void BeginSample(string functionName, Type associatedCollectorType)
+    {
+        if (collectors == null)
+            return;
+        var associatedCollector = collectors.FirstOrDefault(c => c.GetType() == associatedCollectorType);
+        string key;
+        key = associatedCollector != null
+            ? associatedCollector.name + "_" + functionName
+            : functionName;
+
+        var samplingEvent = (ProfilerSamplingEvent)associatedCollector.GetEvents().FirstOrDefault(e => e.GetType() == typeof(ProfilerSamplingEvent));
+        
+        Debug.Assert(samplingEvent != null, "Event is not registered on the provided collectortype. Please go ahead and add in Populate Events");
+        
+        samplingEvent.BeginSample(key);
+    }
+
+    /// <summary>
+    /// End Sample for the provided event.
+    /// </summary>
+    /// <param name="eventName"></param>
+    public void EndSample(string eventName, Type associatedCollectorType)
+    {
+        if (collectors == null)
+            return;
+        var associatedCollector = collectors.FirstOrDefault(c => c.GetType() == associatedCollectorType);
+        var key = associatedCollector != null ? associatedCollectorType.Name + "_" + eventName : eventName;
+        var samplingEvent = (ProfilerSamplingEvent)associatedCollector.GetEvents().FirstOrDefault(e => e.GetType() == typeof(ProfilerSamplingEvent));
+        
+        Debug.Assert(samplingEvent !=null, "No ProfilerSamplingEvent is associated with the provided collector type");
+        
+        samplingEvent.EndSample(key);
+    }
+
+}   
+}
