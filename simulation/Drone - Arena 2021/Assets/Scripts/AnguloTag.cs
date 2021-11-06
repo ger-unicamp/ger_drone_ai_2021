e07a0f9aced64ba250fb8b3e75e38b37952ae267
EltonCN <43186596+EltonCN@users.noreply.github.com>
2021-11-06 18:27:44 -0300

    Atualizado a cena
    Co-Authored-By: brabesf <84363153+brabesf@users.noreply.github.com>

--- simulation/Drone - Arena 2021/Assets/Scripts/AnguloTag.cs
+++ simulation/Drone - Arena 2021/Assets/Scripts/AnguloTag.cs
@@ -9,22 +9,25 @@ using UnityEngine.Perception.Randomization.Randomizers;
 [AddComponentMenu("Perception/RandomizerTags/AnguloTag")]
 public class AnguloTag : RandomizerTag
 {
-    public float minX;  //possibilita colocar maximo angulo e minimo para cada objeto
-    public float maxX;
-    public float minY;
-    public float maxY;
-    public float minZ;
-    public float maxZ;
+    [Tooltip("Rotação mínima para o objeto no eixo X.")] public float minX;  //possibilita colocar maximo angulo e minimo para cada objeto
+    [Tooltip("Posição máxima para o objeto no eixo X.")] public float maxX;
+    [Tooltip("Posição mínima para o objeto no eixo Y.")] public float minY;
+    [Tooltip("Posição máxima para o objeto no eixo Y.")] public float maxY;
+    [Tooltip("Posição mínima para o objeto no eixo Z.")] public float minZ;
+    [Tooltip("Posição máxima para o objeto no eixo Z.")] public float maxZ;
 
     /// <summary>
-    /// Função 
+    /// Função que altera o ângulo espacial dos objetos em função de parâmetros aleatorizados em x, y e z
     /// </summary>
+    /// <param name="xRotation">Variável aleatória entre 0 e 1 que é responsável pela parte aleatorizada da rotação em X.</param>
+    /// <param name="yRotation">Variável aleatória entre 0 e 1 que é responsável pela parte aleatorizada da rotação em Y.</param>
+    /// <param name="zRotation">Variável aleatória entre 0 e 1 que é responsável pela parte aleatorizada da rotação em Z.</param> 
     public void SetRotation(float xRotation, float yRotation, float zRotation)  //acao chamada para cada objeto com o tag pelo "AnguloRandomizer"
     {
         xRotation = (xRotation * (maxX-minX)) + minX;   //baseado nos 3 parametros de 0 a 1 para a randomizacao, atualiza a rotacao do objeto
         yRotation = (yRotation * (maxY-minY)) + minY;
         zRotation = (zRotation * (maxZ-minZ)) + minZ;
-        Vector3 rotationVector = new Vector3(xRotation, yRotation, zRotation);
+        Vector3 rotationVector = new Vector3(xRotation, yRotation, zRotation); //precisa alterar para quatérnio antes de rotacionar
         transform.localRotation = Quaternion.Euler(rotationVector); //funcao do unity pra rotacionar o objeto baseado no novo vetor    
     } 
 }
